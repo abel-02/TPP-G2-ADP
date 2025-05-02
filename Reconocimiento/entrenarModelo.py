b9814d82df88e1dfe8441b781527bfd2cdde9f37
abel-02 <abelaquino02j@gmail.com>
2025-05-02 16:36:44 -0300

    Archivo donde se descargan las dependencias, asi es mas facil

---
+++ Reconocimiento/entrenarModelo.py
@@ -0,0 +1,29 @@
+import cv2
+import os
+import numpy as np
+print(cv2.__version__)
+
+dataPath = 'data'  # Carpeta donde tienes los rostros
+peopleList = os.listdir(dataPath)
+
+labels = []
+facesData = []
+label = 0
+
+# Leer imágenes de entrenamiento
+for person in peopleList:
+    personPath = dataPath + '/' + person
+    for fileName in os.listdir(personPath):
+        imagePath = personPath + '/' + fileName
+        image = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+        facesData.append(image)
+        labels.append(label)
+    label += 1
+
+# Crear y entrenar el modelo
+face_recognizer = cv2.face.LBPHFaceRecognizer.create()
+face_recognizer.train(facesData, np.array(labels))
+
+# Guardar el modelo entrenado
+face_recognizer.write('modeloLBPH.xml')
+print("Modelo entrenado y guardado.")
