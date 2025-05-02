0a6b7d7615bf6984cdb8c33d265228aec5ab1499
abel-02 <abelaquino02j@gmail.com>
2025-05-02 15:41:11 -0300

    Abre frame y muestra la camara en vivo

--- Reconocimiento/almacenarCaras.py
+++ Reconocimiento/almacenarCaras.py
@@ -1,13 +1,38 @@
 #Creo carpetas donde guardo las imagenes
 import os
+import cv2
 
 person = 'usuario'
 dataPath= 'data'
 personPath = dataPath + '/' + person
+
+#obs: (se guardaria las caras de cada persona en cada carpeta)
 
 # Crear carpeta si no existe
 if not os.path.exists(personPath):
     print('Carpeta creada: ', personPath)
     os.makedirs(personPath)
 
-#obs: (se guardaria las caras de cada persona en cada carpeta)
+
+
+#Abre frame que muestra la camara y sale con q
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error al abrir la cámara")
+else:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error al obtener el frame")
+            break
+
+        cv2.imshow("Captura", frame)
+
+        # Salir con la tecla 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
