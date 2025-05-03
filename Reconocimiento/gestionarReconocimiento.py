db8548072b28beb8c92053c94fa01757d046b650
abel-02 <abelaquino02j@gmail.com>
2025-05-03 17:23:41 -0300

    Actualizacion de la version de reconocimiento obsoleta

--- Reconocimiento/gestionarReconocimiento.py
+++ Reconocimiento/gestionarReconocimiento.py
@@ -14,7 +14,7 @@ dataPath = 'data'
 peopleList = os.listdir(dataPath)
 print("Personas reconocidas:", peopleList)
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
