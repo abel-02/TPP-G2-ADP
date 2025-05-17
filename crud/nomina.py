8b762b501404a16ceec14a0ef3710181929ba986
rmontoro94 <rodrigo.montoro@hotmail.com>
2025-05-17 18:43:11 -0300

    Se crea la clase de recibo de sueldo

--- crud/nomina.py
+++ crud/nomina.py
@@ -2,4 +2,4 @@ class ReciboDeSueldo:
     def (id_nomina, id_empleado, nombre, apellido, tipo_identificacion, nro_de_identificacion, fecha_ingreso,
         puesto, departamento, periodo, fecha_de_pago, banco, numero_cuenta, salario_base,
          bono_presentismo, bono_antiguedad, horas_extra, jubiliacion, obra_social, ANSSAL, Ley 19032 , impuesto_a_las_ganancias,
-        aporte_sindical, sueldo_bruto, sueldo_neto)
+        aporte_sindical, sueldo_bruto, sueldo_neto )
